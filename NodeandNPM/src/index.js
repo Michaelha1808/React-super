16a04c50eba1665dcbda70aef14d7ed3e98fe7a1
michael <haminhchi18082002@gmail.com>
2023-08-24 21:47:22 +0700

    webpack series ( 25 - 27 )

--- NodeandNPM/src/index.js
+++ NodeandNPM/src/index.js
@@ -1,4 +1,29 @@
+import 'core-js/modules/es.object.values'
+import 'core-js/modules/es.promise'
+import domHandler from './dom';
 import sum from "./utils";
 import './style/style.css'
 import './style/style.scss'
 console.log(sum(100,20));
+domHandler()
+
+// ES6 Spread Operator
+const person = { name: 'Duoc' }
+const personClone = { ...person }
+console.log('personClone', personClone)
+
+// ES7 Object.values
+console.log('Object.values', Object.values(personClone))
+
+// Promise  Async Await
+const  handle = ()=> new Promise((resolve)=>{
+    setTimeout(() => {
+        resolve(100)
+    }, 1000);
+})
+
+const main = async()=>{
+    const value = await handle()
+    console.log('value', value);
+}
+main()
--- NodeandNPM/webpack.config.js
+++ NodeandNPM/webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
-console.log('__dirname', __dirname);
-console.log('path.resolve', path.resolve());
-console.log('path.resolve(__dirname)', path.resolve(__dirname, 'dist'));
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = (env) => {
+    const basePlugins = [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+    
     const isDevelopment = process.env.NODE_ENV == "development"
+    const plugins = isDevelopment ?  basePlugins : [...basePlugins,new BundleAnalyzerPlugin,]
     return {
         mode: isDevelopment ? 'development' : 'production',
         entry: {
@@ -16,27 +26,43 @@ module.exports = (env) => {
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: '[name].[contenthash].js',
-            clean: true
+            clean: true,
+            assetModuleFilename: '[file]'
         },
-        devtool: isDevelopment ?  'source-map' : false,
+        devtool: isDevelopment ? 'source-map' : false,
         module: {
             rules: [
                 {
                     test: /\.s[ac]ss|css$/,
                     use: [MiniCssExtractPlugin.loader, , 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                  '@babel/preset-env',
+                                  {
+                                    debug: true, // Hiển thị debug lên terminal để dễ debug
+                                    useBuiltIns: 'entry', // Dùng cái này thì đơn giản nhất, không cần import core-js vào code
+                                    corejs: '3.23.4' // nên quy định verson core-js để babel-preset-env nó hoạt động tối ưu
+                                  }
+                                ]
+                              ]
+                        }
+                    }
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+                    type: 'asset/resource'
                 }
+            
             ]
         },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: '[name].[contenthash].css'
-            }),
-            new HtmlWebpackPlugin({
-                title: 'Webpack App',
-                filename: 'index.html',
-                template: 'src/template.html'
-            })
-        ],
+        plugins,
         devServer: {
             static: {
                 directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
